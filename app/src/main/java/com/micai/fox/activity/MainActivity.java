e6148097c2ceb969fd80be8f7bd439be8eadd465
杨金玲 <18363820101@163.com>
2017-12-18 11:41:44 +0800

    测试一下

--- app/src/main/java/com/micai/fox/activity/MainActivity.java
+++ app/src/main/java/com/micai/fox/activity/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        tv.setText("");
+        tv.setText("你好");
     }
 }
